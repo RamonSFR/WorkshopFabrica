1981ebbc7baa86c6e5cea8bae393e9f01c3f161e
Auto Logon <autologon@CSED.LAB>
2023-03-27 17:16:02 -0300

    First Commit

---
+++ Variáveis.py
@@ -0,0 +1,9 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número"))
+
+soma = a + b
+
+if soma == 8:
+    print("A soma é igual a 8")
+else:
+    print("A soma é diferente de 8")
